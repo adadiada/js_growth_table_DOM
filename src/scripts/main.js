948852435103575bccf917467f69101757719e44
Hanna Yatskiv <hanna.ada7@gmail.com>
2025-11-13 08:30:38 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -25,47 +25,56 @@ function updateButtons() {
   removeRowButton.disabled = rowCount <= 2;
   appendColumnButton.disabled = colCount >= 10;
   removeColumnButton.disabled = colCount <= 2;
-
-  appendRowButton.addEventListener('click', () => {
-    if (rowCount >= 10) {
-      return;
-    }
-
-    const newRow = table.rows[0].cloneNode(true);
-
-    table.appendChild(newRow);
-    updateButtons();
-  });
-
-  removeRowButton.addEventListener('click', () => {
-    if (rowCount <= 2) {
-      return;
-    }
-    table.deleteRow(-1);
-    updateButtons();
-  });
-
-  appendColumnButton.addEventListener('click', () => {
-    if (colCount >= 10) {
-      return;
-    }
-
-    for (const row of table.rows) {
-      row.insertCell();
-    }
-    updateButtons();
-  });
-
-  removeColumnButton.addEventListener('click', () => {
-    if (colCount <= 2) {
-      return;
-    }
-
-    for (const row of table.rows) {
-      row.deleteCell(-1);
-    }
-
-    updateButtons();
-  });
 }
+
+appendRowButton.addEventListener('click', () => {
+  const rowCount = table.rows.length;
+
+  if (rowCount >= 10) {
+    return;
+  }
+
+  const newRow = table.rows[0].cloneNode(true);
+
+  table.appendChild(newRow);
+  updateButtons();
+});
+
+removeRowButton.addEventListener('click', () => {
+  const rowCount = table.rows.length;
+
+  if (rowCount <= 2) {
+    return;
+  }
+  table.deleteRow(-1);
+  updateButtons();
+});
+
+appendColumnButton.addEventListener('click', () => {
+  const colCount = table.rows[0].cells.length;
+
+  if (colCount >= 10) {
+    return;
+  }
+
+  for (const row of table.rows) {
+    row.insertCell();
+  }
+  updateButtons();
+});
+
+removeColumnButton.addEventListener('click', () => {
+  const colCount = table.rows[0].cells.length;
+
+  if (colCount <= 2) {
+    return;
+  }
+
+  for (const row of table.rows) {
+    row.deleteCell(-1);
+  }
+
+  updateButtons();
+});
+
 updateButtons();
